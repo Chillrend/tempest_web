56aa8998994c1d483a902798fd7bb6c780cfb8b5
Chillrend <madjavacoder@gmail.com>
2021-03-25 06:21:25 +0700

    innprogress discord webhook

--- scripts/crons/sendDiscordNotifs.js
+++ scripts/crons/sendDiscordNotifs.js
@@ -1,9 +1,56 @@
 const moment = require('moment');
-const { Webhook } = require('discord-webhook-node');
+const axios = require('axios');
 const { data } = require('../../models/events.json');
 
 const timeNow = moment();
 
-const parser = moment('14:23', 'HH:mm');
+require('dotenv').config();
 
-console.log(parser);
+data.forEach((d) => {
+  let isTime = false;
+
+  for (let i = 0; i < d.time.length; i ++) {
+    const time = moment(d.time[i], 'HH:mm');
+    const duration = timeNow.diff(time, 'minutes');
+
+    if (duration < 2) {
+      isTime = true;
+    }
+
+    if (isTime) {
+      // SEND HOOK
+
+      const embedObject = {
+        content: d.tags,
+        embeds: [
+          {
+            title: d.eventName,
+            description: d.eventDesc,
+            color: 12261914,
+            author: {
+              name: 'BPJS Reminder Webhook',
+              url: 'https://madjavacoder.com/',
+            },
+            footer: {
+              text: 'Tempest Bot - 2021',
+            },
+            timestamp: moment().toJSON(),
+            image: {
+              url: d.bg_url,
+            },
+          },
+        ],
+      };
+
+      console.log(JSON.stringify(embedObject));
+
+      const url = d.webhookUrl !== undefined ? d.webhookUrl : process.env.DEFAULT_WEBHOOK_URL;
+
+      axios.post(url, embedObject)
+        .then((r) => console.log(`Successfully sent events! ${JSON.stringify(r)}`))
+        .catch((err) => console.log(`Error ! ${err}`));
+
+      break;
+    }
+  }
+});
